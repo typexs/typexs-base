50a92cf5cf1c40fd028f10e9f92f0cfcd2db0475
Robert Kania <cezaryrk@yahoo.de>
2018-02-04 00:53:09 +0100

    update index.t

--- src/index.ts
+++ src/index.ts
@@ -3,50 +3,55 @@ export * from "commons-base";
 export * from "commons-config";
 export * from "commons-moduls";
 
-export * from "./commands/ModulesCommand";
-export * from "./commands/GenerateCommand";
+export * from  "./commands/ModulesCommand";
+export * from  "./commands/GenerateCommand";
 
-export * from "./api/IActivator";
-export * from "./api/IModule";
+export * from  "./api/IActivator";
+export * from  "./api/IModule";
 
-export * from "./libs/IUrlBase";
-export * from "./libs/logging/Log";
-export * from "./libs/logging/LogEvent";
-export * from "./libs/logging/ILoggerOptions";
+export * from  "./libs/IUrlBase";
+export * from  "./libs/logging/Log";
+export * from  "./libs/logging/LogEvent";
+export * from  "./libs/logging/ILoggerOptions";
 
-export * from "./libs/utils/CryptUtils";
-export * from "./libs/utils/BaseUtils";
-export * from "./libs/utils/FileUtils";
-export * from "./libs/utils/DomainUtils";
+export * from  "./libs/utils/CryptUtils";
+export * from  "./libs/utils/FileUtils";
+export * from  "./libs/utils/BaseUtils";
+export * from  "./libs/utils/DomainUtils";
 
-export * from "./libs/schematics/ISchematicsInfo";
-export * from "./libs/schematics/FileSystemEngineHost";
-export * from "./libs/schematics/SchematicsExecutor";
+export * from  "./libs/schematics/ISchematicsInfo";
+export * from  "./libs/schematics/SimpleRegexCodeModifierHelper";
+export * from  "./libs/schematics/FileSystemEngineHost";
+export * from  "./libs/schematics/ISchematicsOptions";
+export * from  "./libs/schematics/SchematicsExecutor";
+export * from  "./libs/schematics/IFileSystemEngineHost";
 
-export * from "./libs/storage/IStorageOptions";
-export * from "./libs/storage/EntitySchemaType";
-export * from "./libs/storage/EntitySchemaColumnOptions";
-export * from "./libs/storage/StorageRef";
-export * from "./libs/storage/Storage";
-export * from "./libs/storage/EntitySchemaRelationOptions";
-export * from "./libs/storage/ConnectionWrapper";
+export * from  "./libs/storage/IStorageOptions";
+export * from  "./libs/storage/EntitySchemaType";
+export * from  "./libs/storage/EntitySchemaColumnOptions";
+export * from  "./libs/storage/StorageRef";
+export * from  "./libs/storage/Storage";
+export * from  "./libs/storage/EntitySchemaRelationOptions";
+export * from  "./libs/storage/ConnectionWrapper";
 
-export * from "./libs/IKeyValuePair";
-export * from "./libs/IHttpHeaders";
-export * from "./libs/Progress";
+export * from  "./libs/IKeyValuePair";
+export * from  "./libs/IHttpHeaders";
+export * from  "./libs/Progress";
 
-export * from "./libs/queue/IQueue";
-export * from "./libs/queue/IQueueWorkload";
-export * from "./libs/queue/QueueJob";
-export * from "./libs/queue/IAsyncQueueOptions";
-export * from "./libs/queue/IQueueProcessor";
-export * from "./libs/queue/AsyncWorkerQueue";
-export * from "./libs/Runtime";
-export * from "./base/RuntimeLoader";
-export * from "./base/IRuntimeLoaderOptions";
-export * from "./base/cli";
-export * from "./base/MetaArgs";
+export * from  "./libs/queue/IQueue";
+export * from  "./libs/queue/IQueueWorkload";
+export * from  "./libs/queue/QueueJob";
+export * from  "./libs/queue/IAsyncQueueOptions";
+export * from  "./libs/queue/IQueueProcessor";
+export * from  "./libs/queue/AsyncWorkerQueue";
 
-export * from "./Bootstrap";
+export * from  "./libs/Runtime";
 
-export * from "./types";
+export * from  "./base/RuntimeLoader";
+export * from  "./base/IRuntimeLoaderOptions";
+export * from  "./base/MetaArgs";
+export * from  "./base/cli";
+
+export * from  "./Bootstrap";
+
+export * from  "./types";

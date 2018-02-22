f6f1b42a52463ef4406fb70b96ca390190314d6a
Robert Kania <cezaryrk@yahoo.de>
2018-02-22 12:50:42 +0100

    fix travis: add debug

--- test/functional/schematics/schematics.spec.ts
+++ test/functional/schematics/schematics.spec.ts
@@ -75,15 +75,24 @@ class BootstrapGeneralSpec {
     }
 
 
-    let json = require(path.join(workdir , 'package.json'));
+    let data = PlatformUtils.readFileSync(path.join(workdir , 'package.json'));
     let gulpExists = PlatformUtils.fileExist(path.join(workdir , 'gulpfile.ts'));
-    console.log(json);
+    let str:string = null;
+    if(_.isString(data)){
+      str = data;
+    }else{
+      str = data.toString()
+    }
+    console.log(data);
+    console.log(str);
     try{
+
+      let json = JSON.parse(str);
       expect(json.name).to.eq('typexs-gulp-test');
       expect(gulpExists).to.be.true;
 
     }catch(err){
-
+      console.log(data);
       console.error(err);
       expect(false).to.be.true;
     }

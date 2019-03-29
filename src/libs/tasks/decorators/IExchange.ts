72be1c611a169487f93432c932f12f4119a00323
Robert Kania <cezaryrk@yahoo.de>
2019-03-29 15:43:56 +0100

    fix: add type and cardinality to property options

--- src/libs/tasks/decorators/IExchange.ts
+++ src/libs/tasks/decorators/IExchange.ts
@@ -1,5 +1,10 @@
+import {JS_DATA_TYPES} from "commons-schema-api/browser";
+
+
 export interface IExchange {
   name?: string;
   optional?: boolean;
   handle?: (x: any) => any;
+  type?: JS_DATA_TYPES | Function
+  caridnality?: number
 }

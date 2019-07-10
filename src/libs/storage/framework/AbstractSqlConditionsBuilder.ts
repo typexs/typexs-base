8db92193caddabff823889fe163595a1ad82345a
Robert Kania <cezaryrk@yahoo.de>
2019-07-10 16:34:51 +0200

    fix: correct escape in sql conditions builder

--- src/libs/storage/framework/AbstractSqlConditionsBuilder.ts
+++ src/libs/storage/framework/AbstractSqlConditionsBuilder.ts
@@ -1,12 +1,12 @@
 import * as _ from 'lodash';
-import {NotYetImplementedError} from "commons-base/browser";
-import {IEntityRef, IClassRef} from "commons-schema-api/browser";
-import {IConditionJoin} from "./IConditionJoin";
+import {NotYetImplementedError} from 'commons-base/browser';
+import {IClassRef, IEntityRef} from 'commons-schema-api/browser';
+import {IConditionJoin} from './IConditionJoin';
 
 
 export abstract class AbstractSqlConditionsBuilder {
 
-  protected inc: number = 1;
+  protected inc = 1;
 
   protected entityDef: IEntityRef;
 
@@ -40,28 +40,30 @@ export abstract class AbstractSqlConditionsBuilder {
     if (_.isEmpty(condition)) {
       return null;
     }
+    // tslint:disable-next-line:no-shadowed-variable
     let control: any = _.keys(condition).filter(k => k.startsWith('$'));
     if (!_.isEmpty(control)) {
       control = control.shift();
       if (this[control]) {
         return this[control](condition, k, condition[control]);
       } else {
-        throw new NotYetImplementedError()
+        throw new NotYetImplementedError();
       }
     } else if (_.isArray(condition)) {
       return this.$or({'$or': condition}, k);
     } else {
+      // tslint:disable-next-line:no-shadowed-variable
       return _.keys(condition).map(k => {
         if (_.isPlainObject(condition[k])) {
           return this.build(condition[k], k);
         }
-        let key = this.lookupKeys(k);
-        let value = condition[k];
+        const key = this.lookupKeys(k);
+        const value = condition[k];
         if (_.isString(value) || _.isNumber(value) || _.isDate(value)) {
-          return `${key} = '${value}'`
+          return `${key} = ${this.escape(value)}`;
         } else {
           throw new Error(`SQL.build not a plain type ${key} = ${JSON.stringify(value)} (${typeof value})`);
-          //return null;
+          // return null;
         }
 
       }).filter(c => !_.isNull(c)).join(' AND ');
@@ -69,50 +71,58 @@ export abstract class AbstractSqlConditionsBuilder {
   }
 
   escape(str: any) {
+    str = this.addSlashes(str);
     if (_.isString(str)) {
-      str = str.replace(/'/g, "\\'");
+      str = `'${str}'`;
+    }
+    return str;
+  }
+
+  addSlashes(str: any) {
+    if (_.isString(str)) {
+      str = str.replace(/'/g, '\\\'');
     }
     return str;
   }
 
   $eq(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} = '${this.escape(value)}'`
+    const _key = this.lookupKeys(key);
+    return `${_key} = ${this.escape(value)}`;
   }
 
   $ne(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} <> '${this.escape(value)}'`
+    const _key = this.lookupKeys(key);
+    return `${_key} <> ${this.escape(value)}`;
   }
 
   $lt(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} < ${this.escape(value)}`
+    const _key = this.lookupKeys(key);
+    return `${_key} < ${this.escape(value)}`;
   }
 
   $le(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} <= ${this.escape(value)}`
+    const _key = this.lookupKeys(key);
+    return `${_key} <= ${this.escape(value)}`;
   }
 
   $gt(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} > ${this.escape(value)}`
+    const _key = this.lookupKeys(key);
+    return `${_key} > ${this.escape(value)}`;
   }
 
   $ge(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} >= ${this.escape(value)}`
+    const _key = this.lookupKeys(key);
+    return `${_key} >= ${this.escape(value)}`;
   }
 
   $like(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} LIKE '${this.escape(value).replace(/%/g, '%%').replace(/\*/g, '%')}'`
+    const _key = this.lookupKeys(key);
+    return `${_key} LIKE ${this.escape(value).replace(/%/g, '%%').replace(/\*/g, '%')}`;
   }
 
   $in(condition: any, key: string = null, value: any = null) {
-    let _key = this.lookupKeys(key);
-    return `${_key} IN (${value.map((x: any) => this.escape(x)).join(',')})`
+    const _key = this.lookupKeys(key);
+    return `${_key} IN (${value.map((x: any) => this.escape(x)).join(',')})`;
   }
 
   $and(condition: any, key: string = null): string {
--- src/libs/storage/framework/typeorm/TypeOrmSqlConditionsBuilder.ts
+++ src/libs/storage/framework/typeorm/TypeOrmSqlConditionsBuilder.ts
@@ -1,54 +1,54 @@
 import * as _ from 'lodash';
-import {AbstractSqlConditionsBuilder} from "../AbstractSqlConditionsBuilder";
-import {NotYetImplementedError} from "commons-base/browser";
-import {RelationMetadataArgs} from "typeorm/browser/metadata-args/RelationMetadataArgs";
-import {IConditionJoin} from "../IConditionJoin";
-import {TypeOrmPropertyRef} from "./schema/TypeOrmPropertyRef";
-import {IClassRef} from "commons-schema-api/browser";
+import {AbstractSqlConditionsBuilder} from '../AbstractSqlConditionsBuilder';
+import {NotYetImplementedError} from 'commons-base/browser';
+import {RelationMetadataArgs} from 'typeorm/browser/metadata-args/RelationMetadataArgs';
+import {IConditionJoin} from '../IConditionJoin';
+import {TypeOrmPropertyRef} from './schema/TypeOrmPropertyRef';
+import {IClassRef} from 'commons-schema-api/browser';
 
 
 export class TypeOrmSqlConditionsBuilder extends AbstractSqlConditionsBuilder {
 
   lookupKeys(key: string): string {
-    let joins = key.split('.');
+    const joins = key.split('.');
     let tmp: IClassRef = this.entityDef.getClassRef();
     let names: string[] = [this.alias];
     let rootAlias = this.alias;
-    for (let _join of joins) {
-      let prop = tmp.getPropertyRef(_join);
+    for (const _join of joins) {
+      const prop = tmp.getPropertyRef(_join);
       if (prop.isReference()) {
-        let from = tmp;
+        const from = tmp;
         tmp = prop.getTargetRef() ? prop.getTargetRef() : null;
 
-        let relation: RelationMetadataArgs = (<TypeOrmPropertyRef>prop).relation;
-        let join: IConditionJoin = {
+        const relation: RelationMetadataArgs = (<TypeOrmPropertyRef>prop).relation;
+        const join: IConditionJoin = {
           alias: this.createAlias(tmp),
           table: tmp.storingName,
           condition: null
         };
 
-        let conditions: string[] = [];
-        if (relation.relationType == 'one-to-many') {
-          let sourceIdKeyProps = from.getPropertyRefs().filter(f => f.isIdentifier());
-          if (sourceIdKeyProps.length == 1) {
-            let sourceIdKey = sourceIdKeyProps[0].name;
-            let targetIdKey = from.storingName + '' + _.capitalize(sourceIdKey);
-            conditions.push([join.alias + '.' + targetIdKey, rootAlias + '.' + sourceIdKey].join(' = '))
+        const conditions: string[] = [];
+        if (relation.relationType === 'one-to-many') {
+          const sourceIdKeyProps = from.getPropertyRefs().filter(f => f.isIdentifier());
+          if (sourceIdKeyProps.length === 1) {
+            const sourceIdKey = sourceIdKeyProps[0].name;
+            const targetIdKey = from.storingName + '' + _.capitalize(sourceIdKey);
+            conditions.push([join.alias + '.' + targetIdKey, rootAlias + '.' + sourceIdKey].join(' = '));
           } else {
             throw new NotYetImplementedError();
           }
-        } else if (relation.relationType == 'many-to-one') {
-          let targetIdKeyProps = tmp.getPropertyRefs().filter(f => f.isIdentifier());
-          if (targetIdKeyProps.length == 1) {
-            let targetIdKey = targetIdKeyProps[0].name;
-            let sourceIdKey = tmp.storingName + '' + _.capitalize(targetIdKey);
-            conditions.push([join.alias + '.' + targetIdKey, rootAlias + '.' + sourceIdKey].join(' = '))
+        } else if (relation.relationType === 'many-to-one') {
+          const targetIdKeyProps = tmp.getPropertyRefs().filter(f => f.isIdentifier());
+          if (targetIdKeyProps.length === 1) {
+            const targetIdKey = targetIdKeyProps[0].name;
+            const sourceIdKey = tmp.storingName + '' + _.capitalize(targetIdKey);
+            conditions.push([join.alias + '.' + targetIdKey, rootAlias + '.' + sourceIdKey].join(' = '));
           } else {
             throw new NotYetImplementedError();
           }
 
         } else {
-          throw new NotYetImplementedError()
+          throw new NotYetImplementedError();
         }
 
         join.condition = conditions.join(' AND ');
@@ -61,7 +61,7 @@ export class TypeOrmSqlConditionsBuilder extends AbstractSqlConditionsBuilder {
         names.push(prop.name);
       }
     }
-    return names.join('.')
+    return names.join('.');
   }
 
 }

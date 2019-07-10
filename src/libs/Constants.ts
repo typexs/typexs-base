085b3d065cd58d6620998de668f4231ac3582f18
Robert Kania <cezaryrk@yahoo.de>
2019-07-10 16:35:13 +0200

    fix: add missing keys

--- src/libs/Constants.ts
+++ src/libs/Constants.ts
@@ -3,18 +3,21 @@ export const K_CLS_API = 'api';
 export const TYPEXS_NAME = 'typexs';
 export const CONFIG_NAMESPACE = 'typexs';
 export const K_WORKDIR = 'workdir';
+
+
 export const K_CLS_ACTIVATOR = 'activator.js';
 export const K_CLS_BOOTSTRAP = 'bootstrap.js';
 export const K_CLS_COMMANDS = 'commands';
+export const K_CLS_GENERATORS = 'generators';
 export const K_CLS_SCHEDULE_ADAPTER_FACTORIES = 'scheduler-factories';
-
-
 export const K_CLS_STORAGE_SCHEMAHANDLER = 'storage.schemahandler.adapters';
 export const K_CLS_CACHE_ADAPTER = 'cache.adapters';
+export const K_CLS_ENTITIES_DEFAULT = 'entity.default';
 
 
 export const C_EVENTBUS = 'eventbus';
 
+export const APP_SYSTEM_DISTRIBUTED = 'app.system.distributed';
 
 export const XS_P_$COUNT = '$count';
 export const XS_P_$LIMIT = '$limit';

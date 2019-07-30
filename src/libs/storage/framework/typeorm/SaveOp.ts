91ffe9b57d25e68688e5cd9f3e5889140617ae85
Robert Kania <cezaryrk@yahoo.de>
2019-07-30 12:39:58 +0200

    refactor: tslint code cleanup

--- src/libs/storage/framework/typeorm/SaveOp.ts
+++ src/libs/storage/framework/typeorm/SaveOp.ts
@@ -1,14 +1,14 @@
 import * as _ from 'lodash';
 
-import {ISaveOp} from "../ISaveOp";
-import {ConnectionWrapper} from "../../ConnectionWrapper";
-import {StorageEntityController} from "../../StorageEntityController";
-import {ISaveOptions} from "../ISaveOptions";
-import {DataContainer} from "../../DataContainer";
+import {ISaveOp} from '../ISaveOp';
+import {ConnectionWrapper} from '../../ConnectionWrapper';
+import {StorageEntityController} from '../../StorageEntityController';
+import {ISaveOptions} from '../ISaveOptions';
+import {DataContainer} from '../../DataContainer';
 
-import {TypeOrmUtils} from "./TypeOrmUtils";
-import {ObjectsNotValidError} from "../../../exceptions/ObjectsNotValidError";
-import {TypeOrmEntityRegistry} from "./schema/TypeOrmEntityRegistry";
+import {TypeOrmUtils} from './TypeOrmUtils';
+import {ObjectsNotValidError} from '../../../exceptions/ObjectsNotValidError';
+import {TypeOrmEntityRegistry} from './schema/TypeOrmEntityRegistry';
 
 
 export class SaveOp<T> implements ISaveOp<T> {
@@ -26,34 +26,34 @@ export class SaveOp<T> implements ISaveOp<T> {
   }
 
   private isMongoDB() {
-    return this.ec.storageRef.dbType == 'mongodb';
+    return this.ec.storageRef.dbType === 'mongodb';
   }
 
   async run(object: T[] | T, options?: ISaveOptions): Promise<T[] | T> {
     _.defaults(options, {validate: false, raw: false});
-    let isArray = _.isArray(object);
+    const isArray = _.isArray(object);
 
     this.objects = this.prepare(object);
-    let objectsValid: boolean = true;
+    let objectsValid = true;
     if (_.get(options, 'validate', false)) {
       objectsValid = await this.validate();
     }
 
     if (objectsValid) {
-      let resolveByEntityDef = TypeOrmUtils.resolveByEntityDef(this.objects);
-      let entityNames = _.keys(resolveByEntityDef);
+      const resolveByEntityDef = TypeOrmUtils.resolveByEntityDef(this.objects);
+      const entityNames = _.keys(resolveByEntityDef);
       this.c = await this.ec.storageRef.connect();
 
       if (this.isMongoDB()) {
-        let promises = [];
-        for (let entityName of entityNames) {
-          let repo = this.c.manager.getMongoRepository(entityName);
-          let entityDef = TypeOrmEntityRegistry.$().getEntityRefFor(entityName);
-          let propertyDef = entityDef.getPropertyRefs().find(p => p.isIdentifier());
+        const promises = [];
+        for (const entityName of entityNames) {
+          const repo = this.c.manager.getMongoRepository(entityName);
+          const entityDef = TypeOrmEntityRegistry.$().getEntityRefFor(entityName);
+          const propertyDef = entityDef.getPropertyRefs().find(p => p.isIdentifier());
           if (options.raw) {
-            let bulk = repo.initializeOrderedBulkOp();
+            const bulk = repo.initializeOrderedBulkOp();
             resolveByEntityDef[entityName].forEach((e: any) => {
-              if (!e._id && propertyDef.name != '_id') {
+              if (!e._id && propertyDef.name !== '_id') {
                 _.set(e, '_id', _.get(e, propertyDef.name, null));
               }
               bulk.find({_id: e._id}).upsert().replaceOne(e);
@@ -63,14 +63,14 @@ export class SaveOp<T> implements ISaveOp<T> {
 
           } else {
             resolveByEntityDef[entityName].forEach((x: any) => {
-              if (!x._id && propertyDef.name != '_id') {
+              if (!x._id && propertyDef.name !== '_id') {
                 _.set(x, '_id', _.get(x, propertyDef.name, null));
               }
             });
 
-            let p = repo.save(resolveByEntityDef[entityName]).then((x: any) => {
+            const p = repo.save(resolveByEntityDef[entityName]).then((x: any) => {
               resolveByEntityDef[entityName].forEach((x: any) => {
-                if (!x._id && propertyDef.name != '_id') {
+                if (!x._id && propertyDef.name !== '_id') {
                   _.set(x, '_id', _.get(x, propertyDef.name, null));
                 }
               });
@@ -86,14 +86,14 @@ export class SaveOp<T> implements ISaveOp<T> {
         }
 
         if (options.noTransaction) {
-          for (let entityName of entityNames) {
+          for (const entityName of entityNames) {
             await this.c.manager.getRepository(entityName).save(resolveByEntityDef[entityName]);
           }
         } else {
-          let results = await this.c.manager.transaction(async em => {
-            let promises = [];
-            for (let entityName of entityNames) {
-              let p = em.getRepository(entityName).save(resolveByEntityDef[entityName]);
+          const results = await this.c.manager.transaction(async em => {
+            const promises = [];
+            for (const entityName of entityNames) {
+              const p = em.getRepository(entityName).save(resolveByEntityDef[entityName]);
               promises.push(p);
             }
             return Promise.all(promises);
@@ -125,7 +125,7 @@ export class SaveOp<T> implements ISaveOp<T> {
 
 
   private async validate() {
-    let valid: boolean = true;
+    let valid = true;
     await Promise.all(_.map(this.objects, o => new DataContainer(o, TypeOrmEntityRegistry.$())).map(async c => {
       valid = valid && await c.validate();
       if (!this.isMongoDB()) {

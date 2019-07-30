429918737fe4b6b56c27322fc4b8987fc01425e6
Robert Kania <cezaryrk@yahoo.de>
2019-07-30 16:21:59 +0200

    test: typings

--- test/functional/schema_api/entity_id_handling.spec.ts
+++ test/functional/schema_api/entity_id_handling.spec.ts
@@ -1,81 +1,81 @@
 import {suite, test} from 'mocha-typescript';
 import {expect} from 'chai';
 
-import * as _ from "lodash";
-import {TypeOrmEntityRegistry} from "../../../src/libs/storage/framework/typeorm/schema/TypeOrmEntityRegistry";
-import {Car} from "./entities/Car";
+import * as _ from 'lodash';
+import {TypeOrmEntityRegistry} from '../../../src/libs/storage/framework/typeorm/schema/TypeOrmEntityRegistry';
+import {Car} from './entities/Car';
 
-import {ComplexIdsKeys} from "./entities/ComplexIdsKeys";
-import {Expressions} from "commons-expressions";
+import {ComplexIdsKeys} from './entities/ComplexIdsKeys';
+import {Expressions} from 'commons-expressions';
 
 
 @suite('functional/entity_id_handling')
-class Entity_id_handlingSpec {
+class EntityIdHandlingSpec {
 
 
   @test
   async 'resolve entity ids from string'() {
 
-    let registry = TypeOrmEntityRegistry.$();
-    let entityDef = registry.getEntityRefFor(Car);
+    const registry = TypeOrmEntityRegistry.$();
+    const entityDef = registry.getEntityRefFor(Car);
 
-    let conditions = Expressions.parseLookupConditions(entityDef,'1');
+    let conditions = Expressions.parseLookupConditions(entityDef, '1');
     expect(_.has(conditions, 'id')).to.be.true;
     expect(conditions['id']).to.be.eq(1);
 
-
-    conditions = Expressions.parseLookupConditions(entityDef,'id=1');
-    expect(_.has(conditions, 'id')).to.be.true;
-    expect(conditions['id']).to.be.eq(1);
-
-    conditions = Expressions.parseLookupConditions(entityDef,'(id=1),(id=2)');
-    expect(conditions).to.have.length(2);
-    expect(conditions).to.be.deep.eq([{id: 1}, {id: 2}]);
-
-    conditions = Expressions.parseLookupConditions(entityDef,'1,2,3');
+    // TODO move to Expressions
+    // conditions = Expressions.parseLookupConditions(entityDef, 'id=1');
+    // expect(_.has(conditions, 'id')).to.be.true;
+    // expect(conditions['id']).to.be.eq(1);
+    //
+    // conditions = Expressions.parseLookupConditions(entityDef, '(id=1),(id=2)');
+    // expect(conditions).to.have.length(2);
+    // expect(conditions).to.be.deep.eq([{id: 1}, {id: 2}]);
+    //
+    conditions = Expressions.parseLookupConditions(entityDef, '1,2,3');
     expect(conditions).to.have.length(3);
     expect(conditions).to.be.deep.eq([{id: 1}, {id: 2}, {id: 3}]);
 
-    conditions = Expressions.parseLookupConditions(entityDef,'(1),(2),(3)');
+    conditions = Expressions.parseLookupConditions(entityDef, '(1),(2),(3)');
     expect(conditions).to.have.length(3);
     expect(conditions).to.be.deep.eq([{id: 1}, {id: 2}, {id: 3}]);
 
-    let entityDefPKs = registry.getEntityRefFor(ComplexIdsKeys);
-    
+    const entityDefPKs = registry.getEntityRefFor(ComplexIdsKeys);
 
-    conditions = Expressions.parseLookupConditions(entityDefPKs,"inc=1,code='test'");
-    expect(conditions).to.be.deep.eq({inc: 1, code: 'test'});
 
-    conditions = Expressions.parseLookupConditions(entityDefPKs,"(inc=1,code='test'),(inc=2,code='test2')");
-    expect(conditions).to.have.length(2);
-    expect(conditions).to.be.deep.eq([{inc: 1, code: 'test'}, {inc: 2, code: 'test2'}]);
-
-    conditions = Expressions.parseLookupConditions(entityDefPKs,"(1,'test'),(2,'test2')");
-    expect(conditions).to.have.length(2);
-    expect(conditions).to.be.deep.eq([{inc: 1, code: 'test'}, {inc: 2, code: 'test2'}]);
-
-    conditions = Expressions.parseLookupConditions(entityDefPKs,"1,'test'");
-    expect(conditions).to.be.deep.eq({inc: 1, code: 'test'});
+    // conditions = Expressions.parseLookupConditions(entityDefPKs, 'inc=1,code=\'test\'');
+    // expect(conditions).to.be.deep.eq({inc: 1, code: 'test'});
+    //
+    // conditions = Expressions.parseLookupConditions(entityDefPKs, '(inc=1,code=\'test\'),(inc=2,code=\'test2\')');
+    // expect(conditions).to.have.length(2);
+    // expect(conditions).to.be.deep.eq([{inc: 1, code: 'test'}, {inc: 2, code: 'test2'}]);
+    //
+    // conditions = Expressions.parseLookupConditions(entityDefPKs, '(1,\'test\'),(2,\'test2\')');
+    // expect(conditions).to.have.length(2);
+    // expect(conditions).to.be.deep.eq([{inc: 1, code: 'test'}, {inc: 2, code: 'test2'}]);
+    //
+    // conditions = Expressions.parseLookupConditions(entityDefPKs, '1,\'test\'');
+    // expect(conditions).to.be.deep.eq({inc: 1, code: 'test'});
   }
 
   @test
   async 'build entity ids to string'() {
 
-    let registry = TypeOrmEntityRegistry.$();
-    let entityDef = registry.getEntityRefFor(Car);
+    const registry = TypeOrmEntityRegistry.$();
+    const entityDef = registry.getEntityRefFor(Car);
 
-    let str = Expressions.buildLookupConditions(entityDef,{id:1});
-    expect(str).to.be.eq("1");
+    let str = Expressions.buildLookupConditions(entityDef, {id: 1});
+    expect(str).to.be.eq('1');
 
-    str = Expressions.buildLookupConditions(entityDef,[{id:1},{id:2}]);
-    expect(str).to.be.eq("1,2");
+    str = Expressions.buildLookupConditions(entityDef, [{id: 1}, {id: 2}]);
+    expect(str).to.be.eq('1,2');
 
-    let entityDefPKs = registry.getEntityRefFor(ComplexIdsKeys);
-    str = Expressions.buildLookupConditions(entityDefPKs,{inc: 1, code: 'test'});
-    expect(str).to.be.eq("1,'test'");
+    const entityDefPKs = registry.getEntityRefFor(ComplexIdsKeys);
+    str = Expressions.buildLookupConditions(entityDefPKs, {inc: 1, code: 'test'});
+    expect(str).to.be.eq('1,\'test\'');
 
-    str = Expressions.buildLookupConditions(entityDefPKs,[{inc: 1, code: 'test'},{inc: 2, code: 'test2'}]);
-    expect(str).to.be.eq("(1,'test'),(2,'test2')");
+    str = Expressions.buildLookupConditions(entityDefPKs, [{inc: 1, code: 'test'}, {inc: 2, code: 'test2'}]);
+    expect(str).to.be.eq('(1,\'test\'),(2,\'test2\')');
   }
 
 }
--- test/functional/storage/storage_01_general.spec.ts
+++ test/functional/storage/storage_01_general.spec.ts
@@ -1,22 +1,22 @@
-import * as fs from "fs";
-import * as path from "path";
+import * as fs from 'fs';
+import * as path from 'path';
 import * as _ from 'lodash';
-import {suite, test} from "mocha-typescript";
-import {expect} from "chai";
-import {Invoker} from "../../../src/base/Invoker";
-import {IStorageOptions} from "../../../src/libs/storage/IStorageOptions";
-import {StorageRef} from "../../../src/libs/storage/StorageRef";
-import {Bootstrap} from "../../../src/Bootstrap";
-import {Config} from "commons-config";
-import {BeforeInsert, Column, PrimaryColumn} from "typeorm";
-import {X1} from "./entities/X1";
-import {Y1} from "./entities/Y1";
-import {TEST_STORAGE_OPTIONS} from "../config";
-import {Container} from "typedi";
-import {ClassRef, XS_DEFAULT} from "commons-schema-api";
+import {suite, test} from 'mocha-typescript';
+import {expect} from 'chai';
+import {Invoker} from '../../../src/base/Invoker';
+import {IStorageOptions} from '../../../src/libs/storage/IStorageOptions';
+import {StorageRef} from '../../../src/libs/storage/StorageRef';
+import {Bootstrap} from '../../../src/Bootstrap';
+import {Config} from 'commons-config';
+import {BeforeInsert, Column, PrimaryColumn} from 'typeorm';
+import {X1} from './entities/X1';
+import {Y1} from './entities/Y1';
+import {TEST_STORAGE_OPTIONS} from '../config';
+import {Container} from 'typedi';
+import {ClassRef, XS_DEFAULT} from 'commons-schema-api';
 
 @suite('functional/storage/general')
-class Storage_01_generalSpec {
+class StorageGeneralSpec {
 
 
   before() {
@@ -27,24 +27,24 @@ class Storage_01_generalSpec {
 
   @test
   async 'storage options override'() {
-    let opt1: IStorageOptions = {name: 'default', type: 'sqlite', entityPrefix: 'test', connectOnStartup: true};
-    let opt2: IStorageOptions = {name: 'default2', type: 'postgres', connectOnStartup: true};
-    let options: IStorageOptions = _.merge(opt1, opt2);
+    const opt1: IStorageOptions = {name: 'default', type: 'sqlite', entityPrefix: 'test', connectOnStartup: true};
+    const opt2: IStorageOptions = {name: 'default2', type: 'postgres', connectOnStartup: true};
+    const options: IStorageOptions = _.merge(opt1, opt2);
     expect(options).to.be.deep.eq({name: 'default2', type: 'postgres', entityPrefix: 'test', connectOnStartup: true});
   }
 
 
   @test
   async 'storage bootstrap'() {
-    let appdir = path.join(__dirname, 'fake_app');
+    const appdir = path.join(__dirname, 'fake_app');
     let bootstrap = await Bootstrap.configure({
       app: {path: appdir},
       modules: {paths: [__dirname + '/../../..']}
     }).prepareRuntime();
     bootstrap = await bootstrap.activateStorage();
 
-    let storageManager = bootstrap.getStorage();
-    let storage = storageManager.get();
+    const storageManager = bootstrap.getStorage();
+    const storage = storageManager.get();
 
     expect(storage['options']).to.deep.include({
       name: 'default',
@@ -52,8 +52,8 @@ class Storage_01_generalSpec {
       database: ':memory:'
     });
 
-    let entityNames = [];
-    for (let fn of storage['options']['entities']) {
+    const entityNames = [];
+    for (const fn of storage['options']['entities']) {
       entityNames.push((<Function>fn).prototype.constructor.name);
     }
     expect(entityNames).to.be.deep.eq([
@@ -64,28 +64,28 @@ class Storage_01_generalSpec {
     expect(storageRef.name).to.eq(XS_DEFAULT);
 
     const TestEntity = require('./fake_app/entities/TestEntity').TestEntity;
-    let classRef = ClassRef.get(TestEntity, 'dummy');
+    const classRef = ClassRef.get(TestEntity, 'dummy');
     storageRef = storageManager.forClass(classRef);
     expect(storageRef.name).to.eq(XS_DEFAULT);
 
 
-    let classRef2 = storageRef.getClassRef('TestEntity');
+    const classRef2 = storageRef.getClassRef('TestEntity');
     expect(classRef2.getClass()).to.be.eq(TestEntity);
-    let classRef3 = storageRef.getClassRef('test_entity');
+    const classRef3 = storageRef.getClassRef('test_entity');
     expect(classRef2.getClass()).to.be.eq(TestEntity);
     expect(classRef2).to.be.eq(classRef3);
     expect(classRef2.machineName).to.be.eq('test_entity');
     expect(classRef2.storingName).to.be.eq('test_entity');
     expect(classRef2.name).to.be.eq('TestEntity');
 
-    let entityRef_1 = storageRef.getEntityRef('test_entity');
-    let entityRef_2 = storageRef.getEntityRef('TestEntity');
-    let entityRef_3 = storageRef.getEntityRef(TestEntity);
+    const entityRef_1 = storageRef.getEntityRef('test_entity');
+    const entityRef_2 = storageRef.getEntityRef('TestEntity');
+    const entityRef_3 = storageRef.getEntityRef(TestEntity);
     expect(entityRef_1.name).to.be.eq('TestEntity');
     expect(entityRef_2.name).to.be.eq('TestEntity');
     expect(entityRef_3.name).to.be.eq('TestEntity');
 
-    let properties = entityRef_1.getPropertyRefs();
+    const properties = entityRef_1.getPropertyRefs();
     expect(properties).to.have.length(2);
     expect(_.map(properties, p => p.name)).to.deep.eq(['id', 'name']);
 
@@ -101,18 +101,18 @@ class Storage_01_generalSpec {
       id: number;
     }
 
-    let invoker = new Invoker();
+    const invoker = new Invoker();
     Container.set(Invoker.NAME, invoker);
 
-    let storage = new StorageRef(TEST_STORAGE_OPTIONS);
+    const storage = new StorageRef(TEST_STORAGE_OPTIONS);
     await storage.prepare();
 
-    storage.addEntityClass(X, "xtable");
+    storage.addEntityClass(X, 'xtable');
     await storage.reload();
     expect(storage['options'].entities).has.length(1);
 
-    let c = await storage.connect();
-    let q = await c.manager.query('SELECT * FROM sqlite_master WHERE type=\'table\';');
+    const c = await storage.connect();
+    const q = await c.manager.query('SELECT * FROM sqlite_master WHERE type=\'table\';');
     expect(q).has.length(1);
   }
 
@@ -124,22 +124,22 @@ class Storage_01_generalSpec {
       id: number;
     }
 
-    let dbfile = path.join(__dirname, 'testdb01.sqlite');
-    let opts = _.merge(_.clone(TEST_STORAGE_OPTIONS), {database: dbfile});
+    const dbfile = path.join(__dirname, 'testdb01.sqlite');
+    const opts = _.merge(_.clone(TEST_STORAGE_OPTIONS), {database: dbfile});
 
 
-    let invoker = new Invoker();
+    const invoker = new Invoker();
     Container.set(Invoker.NAME, invoker);
 
-    let storage = new StorageRef(opts);
+    const storage = new StorageRef(opts);
     await storage.prepare();
 
-    storage.addEntityClass(X, "xtable");
+    storage.addEntityClass(X, 'xtable');
     await storage.reload();
     expect(storage['options'].entities).has.length(1);
 
-    let c = await storage.connect();
-    let q = await c.manager.query('SELECT * FROM sqlite_master WHERE type=\'table\';');
+    const c = await storage.connect();
+    const q = await c.manager.query('SELECT * FROM sqlite_master WHERE type=\'table\';');
     expect(q).has.length(1);
 
     if (fs.existsSync(dbfile)) {
@@ -157,7 +157,7 @@ class Storage_01_generalSpec {
       @Column()
       txt: string;
 
-      test: boolean = false;
+      test = false;
 
       @BeforeInsert()
       t() {
@@ -166,7 +166,7 @@ class Storage_01_generalSpec {
     }
 
     class Y extends X {
-      test2: boolean = false;
+      test2 = false;
 
       @BeforeInsert()
       tes() {
@@ -174,25 +174,25 @@ class Storage_01_generalSpec {
       }
     }
 
-    let invoker = new Invoker();
+    const invoker = new Invoker();
     Container.set(Invoker.NAME, invoker);
 
-    let storage = new StorageRef(TEST_STORAGE_OPTIONS);
+    const storage = new StorageRef(TEST_STORAGE_OPTIONS);
     await storage.prepare();
 
-    storage.addEntityClass(X, "xtable");
-    storage.addEntityClass(Y, "ytable");
+    storage.addEntityClass(X, 'xtable');
+    storage.addEntityClass(Y, 'ytable');
     await storage.reload();
 
-    let c = await storage.connect();
-    let repo = c.manager.getRepository("xtable");
+    const c = await storage.connect();
+    let repo = c.manager.getRepository('xtable');
     let x = new X();
     x.id = 1;
     x.txt = 'txt';
     x = await repo.save(x);
     expect(x.test).to.be.true;
 
-    repo = c.manager.getRepository("ytable");
+    repo = c.manager.getRepository('ytable');
     let y = new Y();
     y.id = 1;
     y.txt = 'txt';
@@ -205,27 +205,27 @@ class Storage_01_generalSpec {
 
   @test
   async 'typeorm detect listener on fixed added class'() {
-    let opts = _.merge(_.clone(TEST_STORAGE_OPTIONS), {entities: [X1, Y1]});
+    const opts = _.merge(_.clone(TEST_STORAGE_OPTIONS), {entities: [X1, Y1]});
 
-    let invoker = new Invoker();
+    const invoker = new Invoker();
     Container.set(Invoker.NAME, invoker);
 
-    let storage = new StorageRef(opts);
+    const storage = new StorageRef(opts);
     await storage.prepare();
 
-    let c = await storage.connect();
-    let repo = c.manager.getRepository(X1);
+    const c = await storage.connect();
+    const repo = c.manager.getRepository(X1);
     let x = new X1();
     x.id = 1;
     x.txt = 'txt';
     x = await repo.save(x);
     expect(x.test).to.be.true;
 
-    repo = c.manager.getRepository(Y1);
+    const repo2 = c.manager.getRepository(Y1);
     let y = new Y1();
     y.id = 1;
     y.txt = 'txt';
-    y = await repo.save(y);
+    y = await repo2.save(y);
     expect(y.test).to.be.true;
     expect(y.test2).to.be.true;
     await storage.shutdown(true);

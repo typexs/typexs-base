b082a67099ba05a426453e7df25b7a921c5133f2
Robert Kania <cezaryrk@yahoo.de>
2019-11-04 21:56:26 +0100

    fix: redis cache timeout

--- src/adapters/cache/redis/RedisCacheClient.ts
+++ src/adapters/cache/redis/RedisCacheClient.ts
@@ -1,7 +1,7 @@
 import * as _ from 'lodash';
-import {ClientOpts, createClient, RedisClient} from "redis";
-import {IRedisCacheClient} from "./IRedisCacheClient";
-import {ICacheGetOptions, ICacheSetOptions} from "../../../libs/cache/ICacheOptions";
+import {ClientOpts, createClient, RedisClient} from 'redis';
+import {IRedisCacheClient} from './IRedisCacheClient';
+import {ICacheGetOptions, ICacheSetOptions} from '../../../libs/cache/ICacheOptions';
 
 
 export class RedisCacheClient implements IRedisCacheClient {
@@ -10,7 +10,7 @@ export class RedisCacheClient implements IRedisCacheClient {
 
   client: RedisClient;
 
-  private connected: boolean = false;
+  private connected = false;
 
 
   constructor(options: ClientOpts) {
@@ -22,23 +22,22 @@ export class RedisCacheClient implements IRedisCacheClient {
     if (this.connected) {
       return Promise.resolve(this);
     }
-    return new Promise(
-      (resolve, reject) => {
-        let timer = setTimeout(() => {
-          reject(new Error('timeout redis'));
-        }, 5000);
-        const onError = (err: Error) => {
-          reject(err);
-        };
-        this.client = createClient(this.options);
-        this.client.once('error', onError);
-        this.client.once('ready', args => {
-          clearTimeout(timer);
-          this.client.removeListener('error', onError);
-          this.connected = true;
-          resolve(this);
-        });
+    return new Promise((resolve, reject) => {
+      const timer = setTimeout(() => {
+        reject(new Error('timeout redis'));
+      }, 5000);
+      const onError = (err: Error) => {
+        reject(err);
+      };
+      this.client = createClient(this.options);
+      this.client.once('error', onError);
+      this.client.once('ready', args => {
+        clearTimeout(timer);
+        this.client.removeListener('error', onError);
+        this.connected = true;
+        resolve(this);
       });
+    });
   }
 
 
@@ -54,7 +53,7 @@ export class RedisCacheClient implements IRedisCacheClient {
           if (_.isBuffer(reply)) {
             reply = reply.toString();
           }
-          let _value = this.unserialize(reply);
+          const _value = this.unserialize(reply);
           resolve(_value);
         }
       });
@@ -67,7 +66,7 @@ export class RedisCacheClient implements IRedisCacheClient {
 
   unserialize(v: any) {
     try {
-      return JSON.parse(v)
+      return JSON.parse(v);
     } catch (e) {
       return null;
     }
@@ -80,12 +79,16 @@ export class RedisCacheClient implements IRedisCacheClient {
         reject(new Error('no connection'));
       }
 
-      let _value = this.serialize(value);
-      let args: any[] = [key, _value];
+      const _value = this.serialize(value);
+      const args: any[] = [key, _value];
       if (options && options.ttl) {
-        args.push('EX', options.ttl);
-      }
+        if (options.ttl % 1000 === 0) {
+          args.push('EX', Math.round(options.ttl / 1000));
+        } else {
+          args.push('PX', options.ttl);
+        }
 
+      }
       args.push((err: Error, reply: any) => {
         if (err) {
           reject(err);
@@ -93,9 +96,7 @@ export class RedisCacheClient implements IRedisCacheClient {
           resolve(reply);
         }
       });
-
       this.client.set.apply(this.client, args);
-
     });
   }
 
@@ -136,24 +137,24 @@ export class RedisCacheClient implements IRedisCacheClient {
     return new Promise((resolve, reject) => {
       this.client.keys(name, (err, reply) => {
         if (err) {
-          reject(err)
+          reject(err);
         } else {
           if (!_.isEmpty(reply)) {
             this.client.del(reply, (err1, reply1) => {
               if (err1) {
-                reject(err1)
+                reject(err1);
               } else {
                 resolve(reply1);
               }
-            })
+            });
           } else {
             resolve(0);
           }
         }
-      })
-    })
+      });
+    });
 
-    //return null;
+    // return null;
   }
 
 }

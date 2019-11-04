c034768fc37aa82729f3f6798d8aa64c509c8367
Robert Kania <cezaryrk@yahoo.de>
2019-11-04 16:56:14 +0100

    fix: remove gulp task, which are now under @allgemein/packaging

--- src/gulp/clean.ts
+++
@@ -1,7 +0,0 @@
-import * as gulp from 'gulp';
-import * as del from 'del';
-
-
-gulp.task('clean', () => {
-  return del(['./build/**']);
-});
--- src/gulp/compile.ts
+++
@@ -1,9 +0,0 @@
-import * as gulp from 'gulp';
-import * as shell from 'gulp-shell';
-
-
-gulp.task('compile', () => {
-  return gulp.src('package.json', {read: false})
-    .pipe(shell(['tsc']));
-});
-

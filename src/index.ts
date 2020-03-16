0a140aa4673eb72da6247dfd2ac879c6747e1ccd
Robert Kania <cezaryrk@yahoo.de>
2020-03-16 20:17:52 +0100

    fix: uncomment index

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,122 @@
-//
-// export * from './browser';
-//
-// // extern exports
-// export {Container, Inject, Service} from 'typedi';
-// export {Config, IConfigOptions, IConfigData, IFileConfigOptions} from 'commons-config';
-// export {ClassesLoader, ModuleRegistry} from 'commons-moduls';
-// export {PlatformUtils, FileUtils, ClassLoader} from 'commons-base';
-//
-//
-//
-// export * from './api/Tasks.api';
-// export * from './api/ITasksApi';
-//
-// export * from './api/System.api';
-// export * from './api/ISystemApi';
-//
-//
-// export * from './adapters/exchange/config/ConfigExchange';
-// export * from './adapters/exchange/tasks/TasksExchange';
-// export * from './adapters/exchange/filesystem/FileSystemExchange';
-//
-// export * from './adapters/cache/MemoryCacheAdapter';
-// export * from './adapters/cache/RedisCacheAdapter';
-// export * from './adapters/cache/redis/IRedisCacheClient';
-//
-//
-// export * from './adapters/storage/DefaultSchemaHandler';
-// export * from './adapters/storage/SqliteSchemaHandler';
-// export * from './adapters/storage/PostgresSchemaHandler';
-// export * from './adapters/storage/MysqlSchemaHandler';
-// export * from './adapters/storage/MongoDbSchemaHandler';
-//
-//
-// export * from './base/cli';
-// export * from './base/IRuntimeLoaderOptions';
-// export * from './base/RuntimeLoader';
-//
-// export * from './libs/helper/Counter';
-// export * from './libs/helper/Counters';
-//
-// export * from './libs/filesystem/FileReadUtils';
-// export * from './libs/filesystem/IFileStat';
-//
-//
-// export * from './libs/IHttpHeaders';
-// export * from './libs/IKeyValuePair';
-// export * from './libs/IUrlBase';
-// export * from './libs/Progress';
-//
-// export * from './libs/logging/Log';
-// export * from './libs/logging/Console';
-// export * from './libs/logging/LogEvent';
-// export * from './libs/logging/WinstonLoggerJar';
-//
-// export * from './libs/messaging/AbstractEvent';
-// export * from './libs/messaging/AbstractExchange';
-// export * from './libs/messaging/ExchangeMessageRegistry';
-// export * from './libs/messaging/Message';
-// export * from './libs/messaging/IMessageOptions';
-//
-//
-//
-// export * from './libs/queue/AsyncWorkerQueue';
-// export * from './libs/queue/IAsyncQueueOptions';
-// export * from './libs/queue/IQueue';
-// export * from './libs/queue/IQueueProcessor';
-// export * from './libs/queue/IQueueWorkload';
-// export * from './libs/queue/QueueJob';
-//
-// export * from './libs/system/System';
-// export * from './libs/system/INodeInfo';
-//
-// export * from './libs/storage/ConnectionWrapper';
-// export * from './libs/storage/EntitySchemaColumnOptions';
-// export * from './libs/storage/EntitySchemaRelationOptions';
-// export * from './libs/storage/IStorageOptions';
-// export * from './libs/storage/IDBType';
-// export * from './libs/storage/Storage';
-// export * from './libs/storage/StorageRef';
-// export * from './libs/storage/AbstractSchemaHandler';
-// export * from './libs/storage/StorageEntityController';
-//
-//
-// export * from './libs/distributed_storage/IDistributedQueryWorkerOptions';
-// export * from './libs/distributed_storage/DistributedStorageEntityController';
-// export * from './libs/distributed_storage/DistributedOperationFactory';
-// export * from './libs/distributed_storage/DistributedFindOp';
-// export * from './libs/distributed_storage/DistributedQueryResultsEvent';
-// export * from './libs/distributed_storage/DistributedQueryEvent';
-// export * from './libs/distributed_storage/DistributedSaveResultsEvent';
-// export * from './libs/distributed_storage/DistributedSaveEvent';
-//
-//
-// export * from './libs/tasks/NullTaskRef';
-// export * from './libs/tasks/ITask';
-// export * from './libs/tasks/TaskRuntimeContainer';
-// export * from './libs/tasks/TaskRun';
-// export * from './libs/tasks/TaskRunner';
-// export * from './libs/tasks/Constants';
-// export * from './libs/tasks/ITaskRuntimeContainer';
-// export * from './libs/tasks/decorators/Incoming';
-// export * from './libs/tasks/decorators/Outgoing';
-// export * from './libs/tasks/decorators/IExchange';
-// export * from './libs/tasks/decorators/TaskRuntime';
-// export * from './libs/tasks/TaskState';
-// export * from './libs/tasks/TaskRunnerRegistry';
-//
-//
-// export * from './libs/worker/Workers';
-// export * from './libs/worker/IWorkerConfig';
-// export * from './libs/worker/IWorker';
-// export * from './libs/worker/Constants';
-// export * from './libs/worker/WorkerRef';
-//
-//
-// export * from './libs/utils/BaseUtils';
-// export * from './libs/utils/DomainUtils';
-// export * from './libs/utils/ConfigUtils';
-// export * from './libs/utils/MatchUtils';
-//
-// export * from './Bootstrap';
+
+export * from './browser';
+
+// extern exports
+export {Container, Inject, Service} from 'typedi';
+export {Config, IConfigOptions, IConfigData, IFileConfigOptions} from 'commons-config';
+export {ClassesLoader, ModuleRegistry} from 'commons-moduls';
+export {PlatformUtils, FileUtils, ClassLoader} from 'commons-base';
+
+
+
+export * from './api/Tasks.api';
+export * from './api/ITasksApi';
+
+export * from './api/System.api';
+export * from './api/ISystemApi';
+
+
+export * from './adapters/exchange/config/ConfigExchange';
+export * from './adapters/exchange/tasks/TasksExchange';
+export * from './adapters/exchange/filesystem/FileSystemExchange';
+
+export * from './adapters/cache/MemoryCacheAdapter';
+export * from './adapters/cache/RedisCacheAdapter';
+export * from './adapters/cache/redis/IRedisCacheClient';
+
+
+export * from './adapters/storage/DefaultSchemaHandler';
+export * from './adapters/storage/SqliteSchemaHandler';
+export * from './adapters/storage/PostgresSchemaHandler';
+export * from './adapters/storage/MysqlSchemaHandler';
+export * from './adapters/storage/MongoDbSchemaHandler';
+
+
+export * from './base/cli';
+export * from './base/IRuntimeLoaderOptions';
+export * from './base/RuntimeLoader';
+
+export * from './libs/helper/Counter';
+export * from './libs/helper/Counters';
+
+export * from './libs/filesystem/FileReadUtils';
+export * from './libs/filesystem/IFileStat';
+
+
+export * from './libs/IHttpHeaders';
+export * from './libs/IKeyValuePair';
+export * from './libs/IUrlBase';
+export * from './libs/Progress';
+
+export * from './libs/logging/Log';
+export * from './libs/logging/Console';
+export * from './libs/logging/LogEvent';
+export * from './libs/logging/WinstonLoggerJar';
+
+export * from './libs/messaging/AbstractEvent';
+export * from './libs/messaging/AbstractExchange';
+export * from './libs/messaging/ExchangeMessageRegistry';
+export * from './libs/messaging/Message';
+export * from './libs/messaging/IMessageOptions';
+
+
+
+export * from './libs/queue/AsyncWorkerQueue';
+export * from './libs/queue/IAsyncQueueOptions';
+export * from './libs/queue/IQueue';
+export * from './libs/queue/IQueueProcessor';
+export * from './libs/queue/IQueueWorkload';
+export * from './libs/queue/QueueJob';
+
+export * from './libs/system/System';
+export * from './libs/system/INodeInfo';
+
+export * from './libs/storage/ConnectionWrapper';
+export * from './libs/storage/EntitySchemaColumnOptions';
+export * from './libs/storage/EntitySchemaRelationOptions';
+export * from './libs/storage/IStorageOptions';
+export * from './libs/storage/IDBType';
+export * from './libs/storage/Storage';
+export * from './libs/storage/StorageRef';
+export * from './libs/storage/AbstractSchemaHandler';
+export * from './libs/storage/StorageEntityController';
+
+
+export * from './libs/distributed_storage/IDistributedQueryWorkerOptions';
+export * from './libs/distributed_storage/DistributedStorageEntityController';
+export * from './libs/distributed_storage/DistributedOperationFactory';
+export * from './libs/distributed_storage/DistributedFindOp';
+export * from './libs/distributed_storage/DistributedQueryResultsEvent';
+export * from './libs/distributed_storage/DistributedQueryEvent';
+export * from './libs/distributed_storage/DistributedSaveResultsEvent';
+export * from './libs/distributed_storage/DistributedSaveEvent';
+
+
+export * from './libs/tasks/NullTaskRef';
+export * from './libs/tasks/ITask';
+export * from './libs/tasks/TaskRuntimeContainer';
+export * from './libs/tasks/TaskRun';
+export * from './libs/tasks/TaskRunner';
+export * from './libs/tasks/Constants';
+export * from './libs/tasks/ITaskRuntimeContainer';
+export * from './libs/tasks/decorators/Incoming';
+export * from './libs/tasks/decorators/Outgoing';
+export * from './libs/tasks/decorators/IExchange';
+export * from './libs/tasks/decorators/TaskRuntime';
+export * from './libs/tasks/TaskState';
+export * from './libs/tasks/TaskRunnerRegistry';
+
+
+export * from './libs/worker/Workers';
+export * from './libs/worker/IWorkerConfig';
+export * from './libs/worker/IWorker';
+export * from './libs/worker/Constants';
+export * from './libs/worker/WorkerRef';
+
+
+export * from './libs/utils/BaseUtils';
+export * from './libs/utils/DomainUtils';
+export * from './libs/utils/ConfigUtils';
+export * from './libs/utils/MatchUtils';
+
+export * from './Bootstrap';

680b6c02dd2fdda69742669770cd2c2a928e5f52
Robert Kania <cezaryrk@yahoo.de>
2021-05-05 18:19:22 +0200

    build: update build + version

--- .mocharc.js
+++ .mocharc.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // Here's a JavaScript-based config file.
-// If you need conditional logic, you might want to use this type of config.
+// If you need conditional logic, you might want to use this propertyType of config.
 // Otherwise, JSON or YAML is recommended.
 
 module.exports = {

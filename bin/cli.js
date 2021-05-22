a5ed167eb32bfdbf95ed63f6812257b78eba51a4
Robert Kania <cezaryrk@yahoo.de>
2021-05-22 22:39:00 +0200

    refactor: formating

--- bin/cli.js
+++ bin/cli.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env node
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {value: true});
 const fs = require("fs");
 const path = require("path");
 const current = process.cwd();
 var tsMode = false;
 
-if(fs.existsSync(path.join(current,'tsconfig.json'))) {
+if (fs.existsSync(path.join(current, 'tsconfig.json'))) {
   tsMode = true;
   require('ts-node').register({
-    project: path.join(current,'tsconfig.json')
+    project: path.join(current, 'tsconfig.json')
   });
 }
 
 require("reflect-metadata");
-if(fs.existsSync(path.join(current,'package.json'))){
-  var json = require(path.join(current,'package.json'));
-  if(json.name === '@typexs/base'){
-    const cli_1 = require(tsMode ? path.join(current,'src','base','cli') : path.join(current,'base','cli'));
+if (fs.existsSync(path.join(current, 'package.json'))) {
+  var json = require(path.join(current, 'package.json'));
+  if (json.name === '@typexs/base') {
+    const cli_1 = require(tsMode ? path.join(current, 'src', 'base', 'cli') : path.join(current, 'base', 'cli'));
     cli_1.cli();
-  }else{
+  } else {
     const cli_1 = require("@typexs/base");
     cli_1.cli();
   }
-}else{
+} else {
   console.log('no package.json present, create project first');
 }

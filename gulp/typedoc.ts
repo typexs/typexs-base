8a3dd045acf5b80c6d3cbb67688655560188a5e0
Robert Kania <cezaryrk@yahoo.de>
2021-05-23 17:43:37 +0200

    build: disable file assets path correction - doesn't work

--- gulp/typedoc.ts
+++ gulp/typedoc.ts
@@ -11,21 +11,21 @@ gulp.task('typedoc-cloudflare', function () {
   const root = resolve('.');
   const json = JSON.parse(readFileSync(join(root, 'typedoc.json')).toString()) as any;
   // console.log(json);
-  const _copyJson = cloneDeep(json);
+  // const _copyJson = cloneDeep(json);
   const x = typedoc(json);
-  x.on('end', () => {
-    setTimeout(() => {
-      const p = root + '/' + _copyJson.out + '/**/*.html';
-      // console.log(p);
-      const files = glob.sync(p);
-      // console.log(files);
-      for (const f of files) {
-        const data = readFileSync(f).toString('utf-8');
-        const newValue = data.replace(/\"(\.|\/)*assets/g, '\"/assets');
-        writeFileSync(f, newValue, 'utf-8');
-      }
-    }, 15000);
-  });
+  // x.on('end', () => {
+  //   setTimeout(() => {
+  //     const p = root + '/' + _copyJson.out + '/**/*.html';
+  //     // console.log(p);
+  //     const files = glob.sync(p);
+  //     // console.log(files);
+  //     for (const f of files) {
+  //       const data = readFileSync(f).toString('utf-8');
+  //       const newValue = data.replace(/\"(\.|\/)*assets/g, '\"/assets');
+  //       writeFileSync(f, newValue, 'utf-8');
+  //     }
+  //   }, 15000);
+  // });
   return gulp.src(['src/**/*.ts'])
     .pipe(x);
 });
